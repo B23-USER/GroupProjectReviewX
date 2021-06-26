df27c0367ea523580211b51cd61e18d5e091ab51
mustafab11 <mustafab11@yahoo.com>
2021-06-26 22:13:59 +0300

    end of code TC101

--- src/test/java/bilal/TC101.java
+++ src/test/java/bilal/TC101.java
@@ -4,5 +4,7 @@ public class TC101 {
 
     public static void main(String[] args) {
         System.out.println("Helllo World again");
+        int s=12;
+        System.out.println("happyresult for s = " + s);
     }
 }
