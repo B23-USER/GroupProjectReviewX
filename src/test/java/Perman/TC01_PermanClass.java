bd189e67d9180e23b2041ad51bfc66c33fc58e16
Perman <pmammedov@gmail.com>
2021-06-26 15:40:14 -0300

    Work from Perman Branch

--- src/test/java/Perman/TC01_PermanClass.java
+++ src/test/java/Perman/TC01_PermanClass.java
@@ -4,5 +4,8 @@ public class TC01_PermanClass {
 
     public static void main(String[] args) {
         System.out.println("Hi guys");
+        System.out.println("Second commit was done");
+
+
     }
 }
